4f460c0be18bda879ce869ce25acfa74ac03060a
Antonin Lmp <73228586+AntoninLmp@users.noreply.github.com>
2024-02-19 18:29:15 +0100

    Feat : add userManager Entity

---
+++ src/userManagement/domain/customer.entity.ts
@@ -0,0 +1,5 @@
+import { User } from "./user.entity";
+
+export class customer extends User {
+  phoneNumber: string;
+}
---
+++ src/userManagement/domain/orderManager.entity.ts
@@ -0,0 +1,5 @@
+import { User } from "./user.entity";
+
+export class orderManager extends User {
+  assignedParkId: number;
+}

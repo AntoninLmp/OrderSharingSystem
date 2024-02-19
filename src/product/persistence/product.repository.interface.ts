37fcb2527a40184d758f4af7841fe289b6b67813
Kylian Sauvee <kylian@sauvee.com>
2024-02-19 19:53:26 +0100

    feat(app.module): add postgres db and config with env vars

--- src/product/persistence/product.repository.interface.ts
+++
@@ -1,9 +0,0 @@
-import { Product } from "../domain/product.entity";
-
-export interface IProductRepository {
-  add(product: Product): Product;
-  findAll(): Product[];
-  findById(id: number): Product;
-  update(id: number, product: Product): Product;
-  delete(id: number): void;
-}

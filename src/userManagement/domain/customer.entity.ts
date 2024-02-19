fa063aa1364b5b6f9f7cc3701dc241cad3e4f8cb
Antonin Lmp <73228586+AntoninLmp@users.noreply.github.com>
2024-02-19 21:07:50 +0100

    Feat : updated userManagement entities

--- src/userManagement/domain/customer.entity.ts
+++ src/userManagement/domain/customer.entity.ts
@@ -1,5 +1,8 @@
+import { Column, Entity } from "typeorm";
 import { User } from "./user.entity";
 
+@Entity()
 export class customer extends User {
+  @Column()
   phoneNumber: string;
 }
--- src/userManagement/domain/orderManager.entity.ts
+++ src/userManagement/domain/orderManager.entity.ts
@@ -1,5 +1,8 @@
+import { Column, Entity } from "typeorm";
 import { User } from "./user.entity";
 
+@Entity()
 export class orderManager extends User {
+  @Column()
   assignedParkId: number;
 }

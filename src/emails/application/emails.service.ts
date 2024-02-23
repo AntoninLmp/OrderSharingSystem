4657eb4cbb288c2973e93accaf468e8f2c5855f4
Antonin Lampin <lampinantonin@gmail.com>
2024-02-23 19:30:49 +0100

    Feat(emails): added emails service

--- src/emails/application/emails.service.ts
+++ src/emails/application/emails.service.ts
@@ -6,19 +6,31 @@ import { User } from "../../users/domain/user.entity";
 export class EmailsService {
   constructor(private readonly mailerService: MailerService) {}
 
-  async sendUserConfirmation(user: User, token: string) {
-    const url = `example.com/auth/confirm?token=${token}`;
-
-    await this.mailerService.sendMail({
-      to: user.email,
-      // from: '"Support Team" <support@example.com>', // override default from
-      subject: "Welcome to Nice App! Confirm your Email",
-      template: "./confirmation", // `.hbs` extension is appended automatically
-      context: {
-        // ✏️ filling curly brackets with content
-        name: user.name,
-        url,
-      },
-    });
+  async sendUserConfirmation(user: User, amountPaid: number, remainingAmount: number) {
+    try {
+      await this.mailerService.sendMail(
+        {
+          to: user.email,
+          subject: "Confirmation of the payment ✔",
+          text: "welcome",
+          template: "./confirmation", // `.hbs` extension is appended automatically
+          context: {
+            // ✏️ filling curly brackets with content
+            name: user.name,
+            amountPaid: amountPaid,
+            remainingAmount: remainingAmount,
+          },
+        },
+        // FOR DEBUGGING
+        /*).then((success) => {
+          console.log(success);
+        })
+        .catch((err) => {
+          console.log(err);
+        }*/
+      );
+    } catch (error) {
+      throw new Error(`Failed to send email to ${user.email}: ${error.message}`);
+    }
   }
 }
